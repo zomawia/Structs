290d97e5f47189c96a16f696483eae7e41d5f2f7
zomawai.sailo <zomawai.sailo@SEA-8E40F1>
2016-08-29 17:55:37 -0700

    Started problem 5

--- Structures/Project1/Source.cpp
+++ Structures/Project1/Source.cpp
@@ -29,6 +29,14 @@ struct Rectangle
 // 3. Pull the code you wrote for question 2 out into a function that creates and then returns the
 // player the user created.
 
+// 4. Create an array of 5 Player structures. Loop through each Player and ask the user to
+//	input the Player’s name and score and store them in each of the 5 Player structures.Do
+//	this step using the function you wrote in question 3. Then loop through the 5 players and
+//	output each of their names and scores
+
+//5. After completing Question 4, now prompt the user to enter in a name.Loop through the 5
+//Players and find the player with that name.If there is a match, output that player’s score,
+//otherwise report this back to the user that a match could not be found.
 
 Player PlayerCreation()
 {
@@ -47,9 +55,18 @@ Player PlayerCreation()
 
 int main()
 {
-	Player myPlayer = PlayerCreation();
+	Player myPlayer[5];
 
-	printf("You are %s with a score of %d", myPlayer.Name, myPlayer.Score);
+	for (int i = 0; i < 5; ++i)
+	{
+		myPlayer[i] = PlayerCreation();
+	}
+
+	for (int i = 0; i < 5; ++i)
+	{
+		printf("You are %s with a score of %d.\n", myPlayer[i].Name, myPlayer[i].Score);
+	}
+
 	getchar();
 
 }
